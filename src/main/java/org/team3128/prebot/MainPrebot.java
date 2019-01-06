c92a47596c6c2a96d11681afee1926936c3870b0
Ronak Roy <ronak.r123@gmail.com>
2019-01-06 14:42:04 -0800

    Made some changes

--- src/main/java/org/team3128/prebot/MainPrebot.java
+++ src/main/java/org/team3128/prebot/MainPrebot.java
@@ -1,25 +1,12 @@
 package org.team3128.prebot;
 
-import org.team3128.common.NarwhalRobot;
+import edu.wpi.first.wpilibj.RobotBase;
 
-public class MainPrebot extends NarwhalRobot {
-	@Override
-	protected void constructHardware() {
-        
-    }
-    
-    @Override
-    protected void constructAutoPrograms() {
+public final class MainPrebot {
+  private MainPrebot() {
+  }
 
-    }
-
-	@Override
-	protected void setupListeners() {
-		
-	}
-    
-    @Override
-    protected void updateDashboard() {
-        
-    }
+  public static void main(String... args) {
+    RobotBase.startRobot(PrebotRobot::new);
+  }
 }
