d08c47aec9817089955b131e5bee31e258ef1183
Team 3128: Controls <controls@team3128.org>
2019-01-06 14:43:35 -0800

    did more things

--- src/main/java/org/team3128/testbench/main/MainTestBench.java
+++ src/main/java/org/team3128/testbench/main/MainTestBench.java
@@ -1,5 +1,42 @@
 package org.team3128.testbench.main;
 
+
+import org.team3128.common.NarwhalRobot;
+import org.team3128.common.drive.SRXTankDrive;
+import org.team3128.common.hardware.misc.Piston;
+import org.team3128.common.hardware.misc.TwoSpeedGearshift;
+import org.team3128.common.listener.ListenerManager;
+import org.team3128.common.listener.POVValue;
+import org.team3128.common.listener.controllers.ControllerExtreme3D;
+import org.team3128.common.listener.controltypes.Button;
+import org.team3128.common.listener.controltypes.POV;
+import org.team3128.common.narwhaldashboard.NarwhalDashboard;
+import org.team3128.common.util.Constants;
+import org.team3128.common.util.Log;
+import org.team3128.common.util.enums.Direction;
+import org.team3128.common.util.units.Angle;
+import org.team3128.common.util.units.Length;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class MainTestBench extends NarwhalRobot {
 
     public TalonSRX boi1, boi2;
@@ -7,6 +44,7 @@ public class MainTestBench extends NarwhalRobot {
     public Joystick leftJoystick, rightJoystick;
     public double wheelCirc;
     public int lowGearMaxSpeed;
+    public SRXTankDrive drive;
 
     @Override
     protected void constructHardware() {
