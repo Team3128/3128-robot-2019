952be70841268af89783ece98afd36d3e1ea39b4
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-01-19 15:07:46 -0800

    adham+mason

--- src/main/java/org/team3128/prebot/autonomous/Test.java
+++ src/main/java/org/team3128/prebot/autonomous/Test.java
@@ -3,18 +3,20 @@ package org.team3128.prebot.autonomous;
 import org.team3128.common.util.enums.Direction;
 import org.team3128.common.util.units.Length;
 import org.team3128.common.drive.SRXTankDrive;
+import org.team3128.common.util.Log;
+import com.kauailabs.navx.frc.AHRS;
 
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 
 public class Test extends CommandGroup {
-    public Test(SRXTankDrive drive) {
+    public Test(SRXTankDrive drive, AHRS ahrs) {
         
         super();
         final double arc_distance = 8 * Length.ft;
 
-        final float angle = 45f;
+        float angle = 45f;
         final double large_turn_radius = arc_distance * 180 / (angle * Math.PI);
         //final float small_turn_radius = (float) (arc_distance * 160 / (angle * Math.PI));
         //final float small_forward = (float) (1 * Length.ft);
@@ -23,12 +25,42 @@ public class Test extends CommandGroup {
         //addSequential(drive.new CmdMoveForward(small_forward, 10000, false));
 
         //addSequential(drive.new CmdFancyArcTurn(large_turn_radius, angle, 10000, Direction.LEFT, 0.1));
-        int leftPos1 = drive.getLeftMotors().getSelectedSensorPosition();
-        int rightPos1 = drive.getRightMotors().getSelectedSensorPosition();
-        addSequential(drive.new CmdFancierArcTurn(large_turn_radius, angle, 10000, Direction.LEFT, 1));
-        int leftPos2 = drive.getLeftMotors().getSelectedSensorPosition();
-        int rightPos2 = drive.getRightMotors().getSelectedSensorPosition();
+        for(int i = 0; i < 3; i++){
+            if(i == 0){
+                angle = 90f;
+            } else if(i==1){
+                angle = 45f;
+            } else if(i==2){
+                angle = 30f;
+            }
+            double R;
+            double Rleft;
+            double Rright;
+            double s;
+            double wheelbase1;
+            double wheelbase2;
+            int leftPos = drive.getLeftMotors().getSelectedSensorPosition();
+            int rightPos = drive.getRightMotors().getSelectedSensorPosition();
+            double theta = Double.valueOf(ahrs.getYaw());
+            addSequential(drive.new CmdFancierArcTurn(large_turn_radius, angle, 10000, Direction.LEFT, 1));
+            theta = ahrs.getYaw() - theta;
+            theta = theta*(Math.PI/180);
+            leftPos = drive.getLeftMotors().getSelectedSensorPosition() - leftPos;
+            rightPos = drive.getRightMotors().getSelectedSensorPosition() - rightPos;
 
+            s = 0.5*(Double.valueOf(leftPos + rightPos));
+        
+            R = s / theta;
+            Rleft = leftPos / theta;
+            Rright = rightPos / theta;
+
+
+            wheelbase1 = 2*(R - Rright);
+            wheelbase2 = 2*(Rleft - R);
+            Log.info("round: ", Integer.toString(i));
+            Log.info("wb1", Double.toString(wheelbase1));
+            Log.info("wb2", Double.toString(wheelbase1));
+        }
         //addSequential(drive.new CmdMoveForward(small_forward, 10000, false));
 
     }
