76519500126435c4ea1f517b45bec85d367d96ad
danielwang546 <42781202+danielwang546@users.noreply.github.com>
2019-01-27 17:12:34 -0800

    Update MainGromit.java
    Deleted duplicate imports

--- src/main/java/org/team3128/gromit/main/MainGromit.java
+++ src/main/java/org/team3128/gromit/main/MainGromit.java
@@ -36,31 +36,12 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import org.team3128.common.NarwhalRobot;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import org.team3128.common.drive.SRXTankDrive;
-import org.team3128.common.util.Constants;
-import org.team3128.common.util.units.Angle;
-import org.team3128.common.util.units.Length;
-import org.team3128.common.util.Log;
+
 import org.team3128.common.util.RobotMath;
-import org.team3128.common.listener.ListenerManager;
-import org.team3128.common.listener.POVValue;
-import org.team3128.common.listener.controltypes.POV;
-import org.team3128.common.narwhaldashboard.NarwhalDashboard;
-import org.team3128.common.listener.controllers.ControllerExtreme3D;
-import org.team3128.common.listener.controltypes.Button;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
