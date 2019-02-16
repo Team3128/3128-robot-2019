88ed3ce10bc5a305c6b5cd1b1e209514987106bc
Mason Holst <mvhsan@ccom.ucsd.edu>
2019-02-16 13:25:49 -0800

    Preliminary fixing for head-then-arcturn and added cases for left and right sides

--- src/main/java/org/team3128/prebot/autonomous/CmdHeadingThenArc.java
+++ src/main/java/org/team3128/prebot/autonomous/CmdHeadingThenArc.java
@@ -10,23 +10,38 @@ import org.team3128.common.drive.SRXTankDrive;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
+import org.team3128.common.util.Log;
+
 
 public class CmdHeadingThenArc extends CommandGroup {
     public CmdHeadingThenArc(Limelight limelight) {
         SRXTankDrive drive = SRXTankDrive.getInstance();
         LimelightData data = limelight.getValues(20);
-        Direction dir = Direction.RIGHT;
-        // double radius = (Math.pow(data.x(), 2) + Math.pow(data.y(), 2))/(2*data.x());
-        double radius = (Math.pow(3, 2) + Math.pow(10, 2))/(2*3);
-        // double angleRobotToHorizontal = RobotMath.atan(data.y()/data.x());
-        double angleRobotToHorizontal = RobotMath.atan(10/3);
-        // double angleRadiusToHorizontal = RobotMath.acos((radius - data.x())/radius);
-        double angleRadiusToHorizontal = RobotMath.acos((radius - 3)/radius);
+        Direction inPlaceDir, arcDir;
+        double x,z;
+        x = Math.abs(data.x());
+        z = Math.abs(data.z());
+        double radius = (Math.pow(x, 2) + Math.pow(z, 2))/(2*x);
+        double angleRobotToHorizontal = RobotMath.atan(z/x);
+        double angleRadiusToHorizontal = RobotMath.acos((radius - x)/radius);
 
         double angleTargetToTangent = angleRobotToHorizontal - (90 - angleRadiusToHorizontal);
         
+        if(data.x() < 0){
+            inPlaceDir = Direction.RIGHT;
+            arcDir = Direction.LEFT;
+        } else {
+            inPlaceDir = Direction.LEFT;
+            arcDir = Direction.RIGHT;
+        }
 
-        addSequential(drive.new CmdInPlaceTurn(angleTargetToTangent, dir, 0.75, 10000));
-        addSequential(drive.new CmdArcTurn(radius * Length.in, (int) angleRadiusToHorizontal, dir, .75, 10000));
+        Log.info("radius", String.valueOf(radius));
+        Log.info("inPlace angle", String.valueOf(angleTargetToTangent));
+        Log.info("arc angle", String.valueOf(angleRadiusToHorizontal));
+        
+        
+        //if we need to turn right but this angle is positive then we need to turn left
+        addSequential(drive.new CmdInPlaceTurn(angleTargetToTangent, inPlaceDir, 0.75, 10000));
+        addSequential(drive.new CmdArcTurn(radius * Length.in, (int) angleRadiusToHorizontal, arcDir, .75, 10000));
     }
 }
