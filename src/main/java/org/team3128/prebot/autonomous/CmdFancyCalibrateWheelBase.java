4734822c6ef58f2369252cbdba266a8a27a2b314
Jugue123 <43428384+Jugue123@users.noreply.github.com>
2019-02-18 14:17:11 -0800

    added ahrs to auto program

--- src/main/java/org/team3128/prebot/autonomous/CmdFancyCalibrateWheelBase.java
+++ src/main/java/org/team3128/prebot/autonomous/CmdFancyCalibrateWheelBase.java
@@ -12,14 +12,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 
 public class CmdFancyCalibrateWheelBase extends CommandGroup {
-    public CmdFancyCalibrateWheelBase() {    
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.8,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.6,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.4,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.2,3));  
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.8,1.0,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.6,1.0,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.4,1.0,3));
-        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.2,1.0,3));
+    public CmdFancyCalibrateWheelBase(AHRS ahrs) {    
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.8,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.6,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.4,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(1.0,0.2,ahrs,3));  
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.8,1.0,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.6,1.0,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.4,1.0,ahrs,3));
+        addSequential(SRXTankDrive.getInstance().new CmdCalculateWheelbase(0.2,1.0,ahrs,3));
     }
 }
