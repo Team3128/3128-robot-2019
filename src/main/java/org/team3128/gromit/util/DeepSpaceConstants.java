0b92dbc23f507237b49b602b23359240a303ab08
googolplex8 <googolplex8@yahoo.com>
2019-03-02 00:58:07 -0800

    cmd auto optimus prime

---
+++ src/main/java/org/team3128/gromit/util/DeepSpaceConstants.java
@@ -0,0 +1,8 @@
+package org.team3128.gromit.util;
+
+import org.team3128.common.util.units.Angle;
+
+public class DeepSpaceConstants {
+    public static double LOWER_TARGET_TY_THRESHOLD = 20.0* Angle.DEGREES;
+    public static double UPPER_TARGET_TY_THRESHOLD = 20.0* Angle.DEGREES;
+}
