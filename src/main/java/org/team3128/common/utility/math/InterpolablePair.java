783f38a580030a71f63d477e5656b9a67c2b68d8
Adham Elarabawy <adhamelarabawy@gmail.com>
2019-09-22 11:00:32 -0700

    Added Matrix + Interpolation implementations

---
+++ src/main/java/org/team3128/common/utility/math/InterpolablePair.java
@@ -0,0 +1,25 @@
+package org.team3128.common.utility.math;
+
+/**
+ * This class uses a double to interpolate between Interpolable<T>
+ *
+ * @param <T> Class that implements Interpolable<T>
+ */
+public class InterpolablePair<T extends Interpolable<T>> {
+
+	private T value;
+	private double key;
+
+	public InterpolablePair(double key, T value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public double getKey() {
+		return key;
+	}
+
+	public T getValue() {
+		return value;
+	}
+}
---
+++ src/main/java/org/team3128/common/utility/math/InterpolatingDouble.java
@@ -0,0 +1,23 @@
+package org.team3128.common.utility.math;
+
+/**
+ * Class that implements Interpolable for use in InterpolableValue
+ */
+public class InterpolatingDouble implements Interpolable<InterpolatingDouble> {
+
+	private double value;
+
+	public InterpolatingDouble(double value) {
+		this.value = value;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	@Override
+	public InterpolatingDouble interpolate(InterpolatingDouble other, double percentage) {
+		double diff = other.getValue() - value;
+		return new InterpolatingDouble(value + (diff * percentage));
+	}
+}
